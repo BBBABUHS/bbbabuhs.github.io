cbda502be4b6e2533fbdd1c7b48bcd4baf121e3f
BBBABUHS <86612330+BBBABUHS@users.noreply.github.com>
2021-06-30 12:22:48 +0800

    Create 8JWPLAYER.php

---
+++ 8JWPLAYER.php
@@ -0,0 +1,242 @@
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+	box-sizing: border-box;
+	padding: 0px;
+	margin: 0px;
+}
+html, body {
+	height: 100%;
+	margin: 0px;
+}
+.container {
+	height: 100%;
+	background: #000000;
+}
+
+.row {
+	height: 100%;
+}
+.column {
+	float: left;
+	width: 33.333%;
+	padding: 10px;
+	height: 33.333%;
+}
+.row:after {
+	content: "";
+	display: table;
+	clear: both;
+	height: 33.333%;
+}
+
+@media only screen and (max-width: 600px) {
+	.column {
+		float: left;
+		width: 100%;
+		padding: 10px;
+		height: 33.333%;
+	}
+}
+</style>
+<script src="https://cdn.jsdelivr.net/npm/cdnbye@latest"></script>
+<script src="https://ssl.p.jwpcdn.com/player/v/8.20.0/jwplayer.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/cdnbye@latest/dist/jwplayer.hlsjs.provider.min.js"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+</head>
+<body style='margin:0;padding:0; background-color:#000000;'>
+<div class="container">
+<div class="row">
+<div class="column">
+<div id="31"></div>
+</div>
+<div class="column">
+<div id="32"></div>
+</div>
+<div class="column">
+<div id="81"></div>
+</div>
+<div class="column">
+<div id="82"></div>
+</div>
+<div class="column">
+<div id="83"></div>
+</div>
+<div class="column">
+<div id="84"></div>
+</div>
+<div class="column">
+<div id="85"></div>
+</div>
+<div class="column">
+<div id="99"></div>
+</div>
+<div class="column">
+<div id="77"></div>
+</div>
+</div>
+</div>
+</body>
+<script>
+    jwplayer.key = "SJVFRVfnop1/vpohL9lnnhkc8k9PJxEFycJs2w/phUf9lavS";
+    jwplayer('83').setup({
+        file: "https://cdn.hkdtmb.com/hls/83/index.m3u8",
+           width: "100%",
+					aspectratio: "16:9",
+        autostart: true,
+        hlsjsConfig: {
+            debug: false,
+            // Other hlsjsConfig options provided by hls.js
+            p2pConfig: {
+                
+                live: true,        // set as true in live mode
+                // Other p2pConfig options provided by CDNBye
+            }
+        },
+    });
+    jwplayer('31').setup({
+        file: "https://cdn.hkdtmb.com/hls/31/index.m3u8",
+           width: "100%",
+					aspectratio: "16:9",
+        autostart: true,
+        hlsjsConfig: {
+            debug: false,
+            // Other hlsjsConfig options provided by hls.js
+            p2pConfig: {
+                
+                live: true,        // set as true in live mode
+                // Other p2pConfig options provided by CDNBye
+            }
+        },
+    });
+ 
+	    jwplayer('84').setup({
+        file: "https://cdn.hkdtmb.com/hls/84/index.m3u8",
+           width: "100%",
+					aspectratio: "16:9",
+        autostart: true,
+        hlsjsConfig: {
+            debug: false,
+            // Other hlsjsConfig options provided by hls.js
+            p2pConfig: {
+                
+                live: true,        // set as true in live mode
+                // Other p2pConfig options provided by CDNBye
+            }
+        },
+    });
+
+    jwplayer('32').setup({
+        file: "https://cdn.hkdtmb.com/hls/32/index.m3u8",
+           width: "100%",
+					aspectratio: "16:9",
+        autostart: true,
+        hlsjsConfig: {
+            debug: false,
+            // Other hlsjsConfig options provided by hls.js
+            p2pConfig: {
+                
+                live: true,        // set as true in live mode
+                // Other p2pConfig options provided by CDNBye
+            }
+        },
+    });
+    jwplayer('81').setup({
+        file: "https://cdn.hkdtmb.com/hls/81/index.m3u8",
+           width: "100%",
+					aspectratio: "16:9",
+        autostart: true,
+        hlsjsConfig: {
+            debug: false,
+            // Other hlsjsConfig options provided by hls.js
+            p2pConfig: {
+                
+                live: true,        // set as true in live mode
+                // Other p2pConfig options provided by CDNBye
+            }
+        },
+    });
+    jwplayer('85').setup({
+        file: "https://cdn.hkdtmb.com/hls/85/index.m3u8",
+           width: "100%",
+					aspectratio: "16:9",
+        autostart: true,
+        hlsjsConfig: {
+            debug: false,
+            // Other hlsjsConfig options provided by hls.js
+            p2pConfig: {
+                
+                live: true,        // set as true in live mode
+                // Other p2pConfig options provided by CDNBye
+            }
+        },
+    });
+    jwplayer('99').setup({
+        file: "https://cdn.hkdtmb.com/hls/99/index.m3u8",
+           width: "100%",
+					aspectratio: "16:9",
+        autostart: true,
+        hlsjsConfig: {
+            debug: false,
+            // Other hlsjsConfig options provided by hls.js
+            p2pConfig: {
+                
+                live: true,        // set as true in live mode
+                // Other p2pConfig options provided by CDNBye
+            }
+        },
+    });
+    jwplayer('77').setup({
+        file: "https://cdn.hkdtmb.com/hls/77/index.m3u8",
+           width: "100%",
+					aspectratio: "16:9",
+        autostart: true,
+        hlsjsConfig: {
+            debug: false,
+            // Other hlsjsConfig options provided by hls.js
+            p2pConfig: {
+                
+                live: true,        // set as true in live mode
+                // Other p2pConfig options provided by CDNBye
+            }
+        },
+    });
+    jwplayer('32').setup({
+        file: "https://cdn.hkdtmb.com/hls/32/index.m3u8",
+           width: "100%",
+					aspectratio: "16:9",
+        autostart: true,
+        hlsjsConfig: {
+            debug: false,
+            // Other hlsjsConfig options provided by hls.js
+            p2pConfig: {
+                
+                live: true,        // set as true in live mode
+                // Other p2pConfig options provided by CDNBye
+            }
+        },
+    });
+    jwplayer('82').setup({
+        file: "https://cdn.hkdtmb.com/hls/82/index.m3u8",
+           width: "100%",
+					aspectratio: "16:9",
+        autostart: true,
+        hlsjsConfig: {
+            debug: false,
+            // Other hlsjsConfig options provided by hls.js
+            p2pConfig: {
+                
+                live: true,        // set as true in live mode
+                // Other p2pConfig options provided by CDNBye
+            }
+        },
+    });
+   jwplayer().on('play', function () {
+        jwplayer().hls.p2pEngine.on('stats', function ({totalHTTPDownloaded, totalP2PDownloaded}) {
+            // get totalHTTPDownloaded and totalP2PDownloaded here
+        })
+    })
+</script>
+</html>
